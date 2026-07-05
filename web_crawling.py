4b5c1abeb12fd4f852cd0ced401bce0a6ae98221
Saksham-Arora18 <saksham0666@gmail.com>
2026-07-06 01:23:37 +0530

    WEB CRAWLING WITH FASTAPI

---
+++ web_crawling.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+
+@app.get("/news")
+def get_news():
+    url = "https://indianexpress.com/"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    title = []
+
+    for item in soup.find_all("a",class_="topblockNews__sidebarLink"):
+        title.append(item.text)
+
+    
+    return{
+        "news":title[:2]
+    }
